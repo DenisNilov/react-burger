4394e134ee54438f46e73c3e928664b74e024aa6
DenisNilov <denis-nilov@yandex.ru>
2024-03-18 15:29:32 +0300

    Рефакторинг по PropType-2

---
+++ src/components/burger-items-ingredients/burger-items-ingredients.jsx
@@ -0,0 +1,50 @@
+import ConstructorIngredient from '../constructor-ingredient/constructor-ingredient.jsx';
+import { ConstructorElement } from "@ya.praktikum/react-developer-burger-ui-components";
+import { ingredientsPropTypes } from '../../utils/constants.js'
+
+export const BunTop = ({ ingredient }) => {
+    return (
+        <ConstructorElement
+            type="top"
+            isLocked={true}
+            text={`${ingredient.name} (верх)`}
+            price={ingredient.price}
+            thumbnail={ingredient.image}
+        />
+    );
+};
+
+
+export const BunBottom = ({ ingredient }) => {
+    return (
+        <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${ingredient.name} (низ)`}
+            price={ingredient.price}
+            thumbnail={ingredient.image}
+        />
+    );
+};
+
+
+export const IngredientsList = ({ ingredients }) => {
+    return (
+        ingredients.map(ingredient =>
+            <ConstructorIngredient
+                data={ingredient}
+                key={ingredient.id}
+            />))
+}
+
+BunTop.propTypes = {
+    ingredient: ingredientsPropTypes
+}
+
+BunBottom.propTypes = {
+    ingredient: ingredientsPropTypes
+}
+
+IngredientsList.propTypes = {
+    ingredient: ingredientsPropTypes
+}
