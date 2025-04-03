d5c36631338a7239e5042e5f3395a701c1f39f38
DenisNilov <denis-nilov@yandex.ru>
2025-04-03 20:44:02 +0300

    Исправил srore на js

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/app/app';
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
-import { store } from './store/store.jsx';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
